18d01a1aa2e6aba97e49bc1ba86e07e01a197dde
ymerenich1998 <ymerenich1998@gmail.com>
2024-05-13 11:24:59 +0300

    added changes

---
+++ src/app/water_4/page.jsx
@@ -0,0 +1,11 @@
+import Watter from "@/components/watter";
+
+export default function Page() {
+  return (
+    <>
+      <Watter imageUrl="/Frame-1.jpg">
+        <h1>test</h1>
+      </Watter>
+    </>
+  );
+}
